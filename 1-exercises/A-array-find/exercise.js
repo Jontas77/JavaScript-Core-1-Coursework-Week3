5120ed62fffd2a6ae5e5caefe3db34af420c66a7
Jontas77 <mrjbheugh@gmail.com>
2021-02-03 21:45:48 +0200

    Completed 1-exercises folder

--- 1-exercises/A-array-find/exercise.js
+++ 1-exercises/A-array-find/exercise.js
@@ -3,7 +3,10 @@
   Using .find(), we'd like to find the first name which starts with A and is longer than 7 letters.
 */
 
-// write your code here
+const findLongNameThatStartsWithA = (names) => {
+   return names.find(elem => elem.includes("A") && elem.length > 7);
+};
+
 
 var names = [
   "Rakesh",
--- 1-exercises/B-array-some/exercise.js
+++ 1-exercises/B-array-some/exercise.js
@@ -11,6 +11,10 @@ var pairsByIndex = [[0, 3], [1, 2], [2, 1], null, [3, 0]];
 // If there is a null value in the array exit the program with the error code
 // https://nodejs.org/api/process.html#process_process_exit_code
 // process.exit(1);
+let count = null;
+if (pairsByIndex.some(elem => elem === count)) {
+  process.exit(1);
+}
 
 var students = ["Islam", "Lesley", "Harun", "Rukmini"];
 var mentors = ["Daniel", "Irina", "Mozafar", "Luke"];
@@ -20,5 +24,6 @@ var pairs = pairsByIndex.map(function (indexes) {
   var mentor = mentors[indexes[1]];
   return [student, mentor];
 });
+
 
 console.log(pairs);
