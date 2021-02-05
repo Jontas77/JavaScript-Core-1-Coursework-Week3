a3c23de1afa26471e3ebb746edbc37e23481ff82
Jontas77 <68595133+Jontas77@users.noreply.github.com>
2021-02-05 23:56:21 +0200

    Completed exercises & mandatory

--- 2-mandatory/6-lane-names.js
+++ 2-mandatory/6-lane-names.js
@@ -6,7 +6,9 @@
   HINT: string and array methods that could be helpful (indexOf, filter)
 */
 
-function getLanes() {}
+function getLanes(street) {
+  return street.filter((road) => road.indexOf('Lane') !== -1);
+}
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 
